edbc7571e6fe404bfeb9f7655a90995987b584a3
GabrieleLombard01 <Gabriellombardo2001@gmail.com>
2024-01-25 17:33:35 +0100

    open all Allow

--- WebAPIForReact/Program.cs
+++ WebAPIForReact/Program.cs
@@ -15,7 +15,10 @@ namespace WebAPIForReact
             {
                 options.AddPolicy("ReactEndPoint", builder =>
                 {
-                    builder.WithOrigins("http://localhost:5173");
+                    //builder.WithOrigins("http://localhost:5173");
+                    builder.AllowAnyOrigin();
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
                 });
             });
             // ----------------------------------------------
